ff70a398e618bb44ffbe8b6a42cd879ede50c721
Rustamjon Tolipov <rustamxontolipov788@gmail.com>
2021-12-02 14:23:00 +0500

    Update label_spec.rb

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,21 +1,3 @@
-# class Label
-#   attr_accessor :id, :title, :color, :items
-
-#   def initialize(title:, color:)
-#     @id = Random.rand(1..100)
-#     @title = title
-#     @color = color
-#     @items = []
-#   end
-
-#   def add_item(item)
-#     @items << item
-#     item.label = self
-#   end
-# end
-
-# test Label
-
 require_relative '../label'
 
 describe Label do
