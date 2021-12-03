1b166c4a402b65cb4bd3c1c8dad496def06b466d
olipliche <olipliche@gmail.com>
2021-12-03 16:17:50 +0100

    Preserve data by Json

--- game.rb
+++ game.rb
@@ -7,6 +7,25 @@ class Game < Item
     @last_played_at = last_played_at
   end
 
+  def to_s
+    "\n[GAME ID #{@id}]\nMultiplayer: #{@multiplayer}, Last played at: #{@last_played_at}\n#{super}\n"
+  end
+
+  def to_json(*args)
+    super.merge({
+                  JSON.create_id => self.class.name,
+                  'publish_date' => @publish_date,
+                  'multiplayer' => @multiplayer,
+                  'last_played_at' => @last_played_at
+                }).to_json(*args)
+  end
+
+  def self.json_create(object)
+    game = new(Date.parse(object['publish_date']), object['multiplayer'], Date.parse(object['last_played_at']))
+    game.id = object['id']
+    game
+  end
+
   private
 
   def can_be_archived?
