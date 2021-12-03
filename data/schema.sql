24787d683c34cb86d2df204ce6b33be40efd9d60
elmar8287 <L-mar@inbox.ru>
2021-12-03 16:13:18 +0400

    Add Musdic Album table

--- data/schema.sql
+++ data/schema.sql
@@ -17,3 +17,35 @@ CREATE TABLE Author(
   last_name          VARCHAR(100),
   PRIMARY KEY (id)
 );
+
+CREATE TABLE Book(
+  id                INT GENERATED ALWAYS AS IDENTITY,
+  label_id          INT,
+  publish_date      DATE,
+  archived          BOOLEAN,
+  publisher         VARCHAR(100),
+  cover_state       VARCHAR(100),
+  PRIMARY KEY (id),
+  FOREIGN KEY (label_id) REFERENCES Label(id)
+);
+
+CREATE TABLE MusicAlbum(
+  id                INT GENERATED ALWAYS AS IDENTITY,
+  genre_id          INT,
+  publish_date      DATE,
+  archived          BOOLEAN,
+  on_spotify        BOOLEAN,
+  PRIMARY KEY (id),
+  FOREIGN KEY (genre_id) REFERENCES Genre(id)
+);
+
+CREATE TABLE Game(
+  id                INT GENERATED ALWAYS AS IDENTITY,
+  author_id         INT,
+  publish_date      DATE,
+  archived          BOOLEAN,
+  last_played       BOOLEAN,
+  multiplayer       BOOLEAN,
+  PRIMARY KEY (id),
+  FOREIGN KEY (author_id) REFERENCES Author(id)
+);
