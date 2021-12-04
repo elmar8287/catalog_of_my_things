b55efef81f3373541733b18969cc039666814503
olipliche <olipliche@gmail.com>
2021-12-04 02:34:50 +0100

    Update schema.sql

--- book.rb
+++ book.rb
@@ -1,13 +1,31 @@
 require './item'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state
-
   def initialize(publisher, cover_state, publish_date)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
+
+  def to_s
+    "\n[BOOK ID #{@id}]\nPublisher: #{@publisher}, Cover state: #{@cover_state}\n#{super}\n"
+  end
+
+  def to_json(*args)
+    super.merge({
+                  JSON.create_id => self.class.name,
+                  'publisher' => @publisher,
+                  'cover_state' => @cover_state
+                }).to_json(*args)
+  end
+
+  def self.json_create(object)
+    book = new(Date.parse(object['publish_date']), object['publisher'], object['cover_state'])
+    book.id = object['id']
+    book
+  end
+
+  private
 
   def can_be_archived?
     super == true || @cover_state == 'bad'
